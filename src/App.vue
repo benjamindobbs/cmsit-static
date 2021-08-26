9033282b6a1c36c03294a48c782d7c5e54de933c
benjamindobbs <benjamindobbs@gmail.com>
2021-08-26 09:47:22 -0400

    fixing layout

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-container fluid>
     <v-layout align-content-center align-center fill-height>
       <v-col align="center" align-content="center">
-        <iframe id="frame" src="https://docs.google.com/forms/d/e/1FAIpQLSejaFnWhG6ntIhAUpOxhVS3gBPcAidO3lnIEyP61a2IN4hYqA/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0"> Loading…</iframe>      
-        </v-col>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejaFnWhG6ntIhAUpOxhVS3gBPcAidO3lnIEyP61a2IN4hYqA/viewform?embedded=true" width="640" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>        
+      </v-col>
       </v-layout>
   </v-container>
 </template>
