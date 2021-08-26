aa8d30c5643689486837cdc0b613d2b984b3f429
benjamindobbs <benjamindobbs@gmail.com>
2021-08-26 14:16:00 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container fluid>
     <v-layout align-content-center align-center fill-height>
+      <p> this is a test </p>
       <v-col align="center" align-content="center">
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejaFnWhG6ntIhAUpOxhVS3gBPcAidO3lnIEyP61a2IN4hYqA/viewform?embedded=true" width="640" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>        
       </v-col>
